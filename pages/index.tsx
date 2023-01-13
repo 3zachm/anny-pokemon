a63e83432d2cea734d465d50c41a106f912f4814
3zachm <3zachm2@gmail.com>
2023-01-12 16:17:42 -0800

    base template fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import HomeLayout from "../layouts/HomeLayout";
 function Home() {
   return (
     <>
-      <Head></Head>
       <div className="flex min-h-screen flex-col items-center justify-center py-2">
         <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
           <h1 className="text-6xl font-bold">hi</h1>
@@ -19,11 +18,5 @@ function Home() {
 Home.getLayout = function getLayout(page: ReactElement) {
   return <HomeLayout>{page}</HomeLayout>;
 };
-
-export async function getStaticProps() {
-  return {
-    props: {},
-  };
-}
 
 export default Home;
